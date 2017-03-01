82fb4dc0e09f9ca0325bb91421d427fd2ea7df70
RC <rc.chandru@gmail.com>
2017-03-01 12:34:55 +0530

    Show source in DragNDrop story

--- stories/DragDrop.story.js
+++ stories/DragDrop.story.js
@@ -5,28 +5,32 @@ import {Board} from '../src';
 
 const data = require('./data.json');
 
-function handleDragStart(cardId, laneId) {
-  console.log('drag started')
-  console.log(`cardId: ${cardId}`)
-  console.log(`laneId: ${laneId}`)
-}
-
-function handleDragEnd(cardId, sourceLaneId, targetLaneId) {
-  console.log('drag ended')
-  console.log(`cardId: ${cardId}`)
-  console.log(`sourceLaneId: ${sourceLaneId}`)
-  console.log(`targetLaneId: ${targetLaneId}`)
-}
 
 storiesOf('react-trello', module)
 
-  .add('Drag-n-Drop',
-    () => (
-      <Board
+  .addWithInfo('Drag-n-Drop',
+    'A demonstration of onDragStart and onDragEnd hooks',
+    () => {
+    
+      function handleDragStart(cardId, laneId) {
+        console.log('drag started')
+        console.log(`cardId: ${cardId}`)
+        console.log(`laneId: ${laneId}`)
+      }
+
+      function handleDragEnd(cardId, sourceLaneId, targetLaneId) {
+        console.log('drag ended')
+        console.log(`cardId: ${cardId}`)
+        console.log(`sourceLaneId: ${sourceLaneId}`)
+        console.log(`targetLaneId: ${targetLaneId}`)
+      }
+
+      return <Board
         data={data}
         draggable={true}
         onDragStart={handleDragStart}
         onDragEnd={handleDragEnd}
       />
-    ))
+    }
+  )
 
