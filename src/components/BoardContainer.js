c4865f8c8b2cc785db9544599dd54e6984d8595c
RC <rc.chandru@gmail.com>
2017-07-10 23:20:08 +0530

    fix(): Using immutability helper to manage state for within BoardContainer

--- src/components/BoardContainer.js
+++ src/components/BoardContainer.js
@@ -1,9 +1,10 @@
-import React, {Component, PropTypes} from 'react'
-import {BoardDiv} from '../styles/Base'
-import {bindActionCreators} from 'redux'
-import {connect} from 'react-redux'
-import {DragDropContext} from 'react-dnd'
+import React, { Component, PropTypes } from 'react'
+import { BoardDiv } from '../styles/Base'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { DragDropContext } from 'react-dnd'
 import MultiBackend from 'react-dnd-multi-backend'
+import update from 'immutability-helper'
 import HTML5toTouch from 'react-dnd-multi-backend/lib/HTML5toTouch'
 import Lane from './Lane'
 
@@ -11,17 +12,16 @@ const boardActions = require('../actions/BoardActions')
 const laneActions = require('../actions/LaneActions')
 
 class BoardContainer extends Component {
-
-  state = {data: this.props.data}
+  state = { data: this.props.data }
 
   wireEventBus = () => {
     let eventBus = {
-      publish: (event) => {
+      publish: event => {
         switch (event.type) {
           case 'ADD_CARD':
-            return this.props.actions.addCard({laneId: event.laneId, card: event.card})
+            return this.props.actions.addCard({ laneId: event.laneId, card: event.card })
           case 'REMOVE_CARD':
-            return this.props.actions.removeCard({laneId: event.laneId, cardId: event.cardId})
+            return this.props.actions.removeCard({ laneId: event.laneId, cardId: event.cardId })
           case 'REFRESH_BOARD':
             return this.props.actions.loadBoard(event.data)
         }
@@ -30,36 +30,40 @@ class BoardContainer extends Component {
     this.props.eventBusHandle(eventBus)
   }
 
-  componentWillMount () {
+  componentWillMount() {
     this.props.actions.loadBoard(this.props.data)
     if (this.props.eventBusHandle) {
       this.wireEventBus()
     }
   }
 
-  componentWillReceiveProps (nextProps) {
-    if (nextProps.data.lanes) {
-      const dataToUpdate = this.state.data
-      dataToUpdate.lanes = nextProps.data.lanes
-      this.setState({data: dataToUpdate})
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.lanes) {
+      const newData = update(this.state.data, { lanes: { $set: nextProps.lanes } })
+
+      this.setState({ data: newData })
       this.props.onDataChange && this.props.onDataChange(nextProps.data)
     }
   }
 
-  render () {
-    const {data} = this.state
-    return <BoardDiv>
-      {
-        data.lanes.map((lane) => {
-          const {id, ...otherProps} = lane
-          const {tagStyle, draggable, handleDragStart, handleDragEnd, onCardClick, onLaneScroll, laneSortFunction} = this.props
-          return <Lane key={`${id}`}
-            id={id}
-            {...otherProps}
-            {...{tagStyle, draggable, handleDragStart, handleDragEnd, onCardClick, onLaneScroll, laneSortFunction}}
-          />
+  render() {
+    const { data } = this.state
+    return (
+      <BoardDiv>
+        {data.lanes.map(lane => {
+          const { id, ...otherProps } = lane
+          const { tagStyle, draggable, handleDragStart, handleDragEnd, onCardClick, onLaneScroll, laneSortFunction } = this.props
+          return (
+            <Lane
+              key={`${id}`}
+              id={id}
+              {...otherProps}
+              {...{ tagStyle, draggable, handleDragStart, handleDragEnd, onCardClick, onLaneScroll, laneSortFunction }}
+            />
+          )
         })}
-    </BoardDiv>
+      </BoardDiv>
+    )
   }
 }
 
@@ -75,10 +79,10 @@ BoardContainer.propTypes = {
   onDataChange: PropTypes.func
 }
 
-const mapStateToProps = (state) => {
-  return state.lanes ? {data: state} : {}
+const mapStateToProps = state => {
+  return state.lanes ? { lanes: state.lanes } : {}
 }
 
-const mapDispatchToProps = (dispatch) => ({actions: bindActionCreators({...boardActions, ...laneActions}, dispatch)})
+const mapDispatchToProps = dispatch => ({ actions: bindActionCreators({ ...boardActions, ...laneActions }, dispatch) })
 
 export default connect(mapStateToProps, mapDispatchToProps)(DragDropContext(MultiBackend(HTML5toTouch))(BoardContainer))
