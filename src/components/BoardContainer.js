7c5c64d312d2d6cdb4f86c018f840cff6ab0cf59
lucas.freire <lucas.freire@ubk.cz>
2018-05-28 14:28:16 +0200

    Fixing getting undefined payload in onDragStart function

--- src/components/BoardContainer.js
+++ src/components/BoardContainer.js
@@ -64,6 +64,7 @@ class BoardContainer extends Component {
 
   onDragStart = (index, payload) => {
     const {handleLaneDragStart} = this.props
+    payload = payload || index.payload
     handleLaneDragStart(payload.id)
   }
 
