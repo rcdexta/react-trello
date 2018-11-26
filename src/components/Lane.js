f0209302270f9425057674ce2f3a2fb5b0d56de0
Ahmet Cetin <ahmet@cetin.info>
2018-11-26 21:23:14 +0100

    Marking individual card not to be draggable
    if card object has property notDraggable = true, then that particular card won't be draggable. Closes #131

--- src/components/Lane.js
+++ src/components/Lane.js
@@ -183,7 +183,7 @@ class Lane extends Component {
           {...card}
         />
       )
-      return draggable && cardDraggable ? (
+      return draggable && cardDraggable && !card.notDraggable ? (
         <Draggable key={card.id}>{cardToRender}</Draggable>
       ) : (
         <span key={card.id}>{cardToRender}</span>
