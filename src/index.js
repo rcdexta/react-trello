5c659852dc06d668f1c3f439b0bbd35c41e08469
Ahmet Cetin <ahmet@cetin.info>
2018-11-27 19:41:36 +0100

    export NewCard component
    It would help user-created NewCard component for styles, and methods

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import Board from './components/Board'
 import Tag from './components/Tag'
 import Lane from './components/Lane'
 import Card from './components/Card'
+import NewCard from './components/NewCard'
 import Draggable from './dnd/Draggable'
 import Container from './dnd/Container'
 
-export {Board, Tag, Lane, Card}
+export {Board, Tag, Lane, Card, NewCard}
 export {Container, Draggable}
 export default Board
