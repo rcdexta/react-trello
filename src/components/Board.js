77f66398494f006adb4df93d1e2a7fba724bc1ae
RC <rc.chandru@gmail.com>
2018-12-29 19:03:13 -0800

    fix(IE 11 Compatibility): Use createGlobalStyle instead of injectGlobal to inject global css styles
    https://github.com/rcdexta/react-trello/issues/152

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,17 +1,18 @@
 import React, {Component} from 'react'
 import BoardContainer from './BoardContainer'
 import {Provider} from 'react-redux'
-import {createStore, applyMiddleware} from 'redux'
+import {applyMiddleware, createStore} from 'redux'
 import boardReducer from '../reducers/BoardReducer'
 import logger from 'redux-logger'
-import uuidv1 from "uuid/v1"
+import uuidv1 from 'uuid/v1'
+import {GlobalStyle} from '../styles/Base'
 
 const middlewares = process.env.REDUX_LOGGING ? [logger] : []
 
 export default class Board extends Component {
   constructor() {
     super()
-    this.store = this.getStore();
+    this.store = this.getStore()
     this.id = uuidv1()
   }
 
@@ -23,7 +24,10 @@ export default class Board extends Component {
   render() {
     return (
       <Provider store={this.store}>
-        <BoardContainer className="react-trello-board" {...this.props} id={this.id}/>
+        <>
+          <GlobalStyle />
+          <BoardContainer className="react-trello-board" {...this.props} id={this.id} />
+        </>
       </Provider>
     )
   }
--- src/styles/Base.js
+++ src/styles/Base.js
@@ -1,6 +1,6 @@
-import styled, {injectGlobal} from 'styled-components'
+import styled, {createGlobalStyle} from 'styled-components'
 
-injectGlobal`
+export const GlobalStyle = createGlobalStyle`
   .comPlainTextContentEditable {
     -webkit-user-modify: read-write-plaintext-only;
   }
