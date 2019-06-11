7450656f0aa9a9f05ae37be3e17ccf93df2a9190
Danil Pismenny <danil@brandymint.ru>
2019-06-11 23:10:13 +0300

    Add i18n Story

--- stories/I18n.story.js
+++ stories/I18n.story.js
@@ -1,28 +1,37 @@
-import React from 'react'
+import React, {Component, Suspense} from 'react'
 import {storiesOf} from '@storybook/react'
+import { useTranslation, I18nextProvider } from 'react-i18next';
 
 import Board from '../src'
 import defaultTranslation from '../src/helpers/defaultTranslation'
+import i18n from './i18n'
 
-const data = require('./data/base.json')
 const smallData = require('./data/data-sort')
 
-const customTranslation = (key) => {
-  return '*' + defaultTranslation(key) + '*'
+const customTranslation = (key) => '*' + defaultTranslation(key) + '*'
+
+const I18nBoard = () => {
+  const { t } = useTranslation()
+  return (
+    <div>
+      <div>
+        <button onClick={() => i18n.changeLanguage('en')}>English</button>
+        <button onClick={() => i18n.changeLanguage('ru')}>Русский</button>
+      </div>
+      <Board data={smallData} t={t} editable canAddLanes draggable />
+    </div>
+  )
 }
 
-storiesOf('Internationalization (I81n)', module).add(
-  'Custom texts',
-  () => {
-    return (
-      <Board
-        data={smallData}
-        t={customTranslation}
-        editable
-        canAddLanes
-        draggable
-        />
-    )
-  },
-  {info: 'Have custom text titles'}
-)
+storiesOf('I81n', module)
+  .addDecorator(story => <I18nextProvider i18n={i18n}>{story()}</I18nextProvider>)
+  .add(
+    'Custom texts',
+    () =>  <Board data={smallData} t={customTranslation} editable canAddLanes draggable /> ,
+    {info: 'Have custom text titles'}
+  )
+  .add(
+    'Using i18next',
+    () => <I18nBoard /> ,
+    {info: 'Availability to switching between languages'}
+  )
