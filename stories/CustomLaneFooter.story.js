fb20de9f70c770ffac09ad6907efef83d7beb56b
Danil Pismenny <danil@brandymint.ru>
2019-06-30 15:49:21 +0300

    Add LaneFooter storybook

---
+++ stories/CustomLaneFooter.story.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {storiesOf} from '@storybook/react'
+
+import Board from '../src'
+
+const data = require('./data/collapsible.json')
+
+const LaneFooter = ({onClick, collapsed}) => <div onClick={onClick}>{collapsed ? 'click to expand' : 'click to collapse'}</div>
+
+storiesOf('Custom Components', module).add('Custom LaneFooter', () => <Board collapsibleLanes components={{LaneFooter: LaneFooter}} data={data} />)
