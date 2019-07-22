9a0e0ad23ea5577dc0e255a23129f194090be9e9
Danil Pismenny <danil@brandymint.ru>
2019-07-22 16:53:25 +0300

    [#281] Add improved new lane title editor

--- src/components/NewLaneForm.js
+++ src/components/NewLaneForm.js
@@ -1,39 +1,57 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import {LaneTitle, NewLaneButtons, Section} from 'rt/styles/Base'
-import {AddButton, CancelButton} from 'rt/styles/Elements'
-import EditableLabel from 'rt/widgets/EditableLabel'
+import { LaneTitle, NewLaneButtons, Section } from 'rt/styles/Base'
+import { AddButton, CancelButton } from 'rt/styles/Elements'
+import NewLaneTitleEditor from 'rt/widgets/NewLaneTitleEditor'
+import ClickOutside from 'react-click-outside'
 
-class NewLaneForm extends Component {
-  updateField = (field, value) => {
-    this.setState({[field]: value})
+class NewLane extends Component {
+  handleSubmit = () => {
+    this.props.onAdd({ title: this.getValue() })
   }
 
-  handleAdd = () => {
-    this.props.onAdd(this.state)
+  getValue = () => this.refInput.getValue()
+
+  onClickOutside = (a,b,c) => {
+    if (this.getValue().length > 0) {
+      this.handleSubmit()
+    } else {
+      this.props.onCancel()
+    }
   }
 
   render() {
-    const {onCancel, t} = this.props
+    const { onCancel, t } = this.props
     return (
+      <ClickOutside onClickOutside={this.onClickOutside}>
         <Section>
           <LaneTitle>
-            <EditableLabel placeholder={t('placeholder.title')} onChange={val => this.updateField('title', val)} autoFocus/>
+            <NewLaneTitleEditor
+              ref={ref => (this.refInput = ref)}
+              placeholder={t('placeholder.title')}
+              onCancel={this.props.onCancel}
+              onSave={this.handleSubmit}
+              resize='vertical'
+              border
+              autoFocus
+            />
           </LaneTitle>
           <NewLaneButtons>
-            <AddButton onClick={this.handleAdd}>{t('button.Add lane')}</AddButton>
+             <AddButton onClick={this.handleSubmit}>{t('button.Add lane')}</AddButton>
             <CancelButton onClick={onCancel}>{t('button.Cancel')}</CancelButton>
           </NewLaneButtons>
         </Section>
+      </ClickOutside>
     )
   }
 }
 
-NewLaneForm.propTypes = {
+NewLane.propTypes = {
   onCancel: PropTypes.func.isRequired,
   onAdd: PropTypes.func.isRequired,
-  t: PropTypes.func.isRequired
+  t: PropTypes.func.isRequired,
 }
-NewLaneForm.defaultProps = {}
 
-export default NewLaneForm
+NewLane.defaultProps = {}
+
+export default NewLane
