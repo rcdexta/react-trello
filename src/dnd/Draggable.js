91fe83610482922d014a6249033c7743f3c205b7
RC <rc.chandru@gmail.com>
2020-03-01 09:34:34 -0800

    Refer to trello-smooth-dnd as the drag-n-drop core library

--- src/dnd/Draggable.js
+++ src/dnd/Draggable.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
-import {constants} from 'smooth-dnd'
+import {constants} from 'trello-smooth-dnd'
 const {wrapperClass} = constants
 
 class Draggable extends Component {
