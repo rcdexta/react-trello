d66af37122d2a8d9441ac01325618f3e8e0ad183
Rubens Flinco <contato@rubensflinco.com.br>
2023-03-10 15:40:52 -0300

    Create translation pt-br

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,10 +1,13 @@
 // i18next support structure
 
 export default {
-  en: {
+  "en": {
     translation: require('./en/translation.json')
   },
-  ru: {
+  "ru": {
     translation: require('./ru/translation.json')
+  },
+  "pt-br": {
+    translation: require('./pt-br/translation.json')
   }
 }
